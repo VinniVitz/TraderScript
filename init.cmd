a43d6c4f582cd3de7ba35a88cc3e6b320790737f
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2021-11-17 23:19:26 +0100

    Update build

--- init.cmd
+++ init.cmd
@@ -3,7 +3,7 @@ set docker_image=
 set /p docker_image=Enter a name for the docker image [trading-coach]:
 if "x%docker_image%x" == "xx" set docker_image=trading-coach
 docker build -t %docker_image% .\data
-xcopy .\data\startup.cmd .
+xcopy ..\data\startup.cmd .
 rmdir /s /q data
-del init.sh
 del init.cmd
+del init.sh
