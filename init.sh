f586402307659777040f20638171a5a98923500f
VinniVitz <34209870+VinniVitz@users.noreply.github.com>
2021-11-19 15:20:58 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-read -p "Enter a name for the docker image [trading-coach]:" docker_image
-docker_image=${docker_image:-"trading-coach"}
+read -p "Enter a name for the docker image [traderscript]:" docker_image
+docker_image=${docker_image:-"traderscript"}
 docker build -t $docker_image ./data
 cp ./data/startup.sh .
 rm -r data
